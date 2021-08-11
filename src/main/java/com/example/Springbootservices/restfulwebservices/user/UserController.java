79594a130f01f9d4a4456392a9d5912d60947602
Surya <surya.sukumar@sixt.com>
2021-08-11 22:41:46 +0530

    Post mapping return status and Location of user added

--- src/main/java/com/example/Springbootservices/restfulwebservices/user/UserController.java
+++ src/main/java/com/example/Springbootservices/restfulwebservices/user/UserController.java
@@ -1,9 +1,11 @@
 package com.example.Springbootservices.restfulwebservices.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import java.lang.reflect.Array;
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -22,9 +24,13 @@ public class UserController {
         return userDaoService.findOne(id);
     }
 
-    @PostMapping("/users/create")
-    public void createUser(@RequestBody User user){
-        userDaoService.save(user);
+    @PostMapping("/users")
+    public ResponseEntity<Object> createUser(@RequestBody User user){
+        User savedUser = userDaoService.save(user);
+
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(savedUser.getId()).toUri();
+        //return status and the user added
+        return ResponseEntity.created(location).build();
     }
 
 
