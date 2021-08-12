1a305ed10df4598213d5be29719c98e96a84131c
Surya <surya.sukumar@sixt.com>
2021-08-12 16:48:41 +0530

    Internationalization of Web Service

--- src/main/java/com/example/Springbootservices/restfulwebservices/RestfulWebServicesApplication.java
+++ src/main/java/com/example/Springbootservices/restfulwebservices/RestfulWebServicesApplication.java
@@ -2,6 +2,14 @@ package com.example.Springbootservices.restfulwebservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
+
 
 @SpringBootApplication
 public class RestfulWebServicesApplication {
@@ -10,6 +18,20 @@ public class RestfulWebServicesApplication {
 
 		SpringApplication.run(RestfulWebServicesApplication.class, args);
 
+	}
+
+	@Bean//What message to display
+	public LocaleResolver localeResolver(){
+		SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+		localeResolver.setDefaultLocale(Locale.US);
+		return localeResolver;
+	}
+
+	@Bean//configuring message source
+	public ResourceBundleMessageSource bundleMessageSource(){
+		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+		messageSource.setBasename("messages");
+		return messageSource;
 	}
 
 }
--- src/main/java/com/example/Springbootservices/restfulwebservices/user/UserController.java
+++ src/main/java/com/example/Springbootservices/restfulwebservices/user/UserController.java
@@ -1,6 +1,7 @@
 package com.example.Springbootservices.restfulwebservices.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,12 +15,16 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import javax.validation.Valid;
 import java.net.URI;
 import java.util.List;
+import java.util.Locale;
 
 @RestController
 public class UserController {
 
     @Autowired
     private UserDaoService userDaoService;
+
+    @Autowired
+    private MessageSource messageSource;
 
     @GetMapping("/users")
     public List<User> users(){
@@ -58,6 +63,11 @@ public class UserController {
         if(user==null)
             throw new UserNotFoundException("id-"+id);
         return user;
+    }
+
+    @GetMapping("/hello-world-internationalized")
+    public String helloWorldInternationalized(@RequestHeader(name = "Accept-Language", required = false) Locale locale){
+        return messageSource.getMessage("good.morning.message",null,locale);
     }
 
 }
