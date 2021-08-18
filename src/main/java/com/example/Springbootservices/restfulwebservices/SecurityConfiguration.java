9471c30d6d4ece0fa3d72e32ad149fcaa6f17c4f
Surya <surya.sukumar@sixt.com>
2021-08-18 14:22:38 +0530

    Fixed 403 forbidden request in POST

--- src/main/java/com/example/Springbootservices/restfulwebservices/SecurityConfiguration.java
+++ src/main/java/com/example/Springbootservices/restfulwebservices/SecurityConfiguration.java
@@ -1,19 +1,25 @@
 package com.example.Springbootservices.restfulwebservices;
-import io.swagger.models.HttpMethod;
+import com.example.Springbootservices.restfulwebservices.user.UserRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
+import javax.servlet.http.HttpServletResponse;
 import java.util.Arrays;
 
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
 
     @Override
     public void configure(WebSecurity web) throws Exception {
@@ -22,12 +28,26 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
             .antMatchers("/h2-console/**");
     }
     @Override
-    protected void configure(HttpSecurity http) throws Exception{
-        http
-                .httpBasic().and()
-                .authorizeRequests()
-                .antMatchers(org.springframework.http.HttpMethod.POST, "/jpa/users").permitAll()
-                .antMatchers(org.springframework.http.HttpMethod.POST,"/jpa/users/{id}/posts").permitAll();
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+        auth.inMemoryAuthentication()
+                .withUser("surya").password("{noop}secret").roles("");
+
+
     }
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http
+                //HTTP Basic authentication
+                .httpBasic()
+                .and()
+                .authorizeRequests()
+                .antMatchers(HttpMethod.GET, "/jpa/users/**").hasRole("")
+                .antMatchers(HttpMethod.POST, "/jpa/users/**").hasRole("")
+                .and()
+                .csrf().disable();
+    }
+
 
 }
